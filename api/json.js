89091715d2753e0c434a5b558ca300138ebd8d00
muhammadsohaibali <your-email@example.com>
2025-03-23 16:04:56 +0500

    update

--- api/json.js
+++ api/json.js
@@ -2,7 +2,7 @@ import jsonServer from 'json-server';
 import path from 'path';
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(process.cwd(), 'src/blog_db.json'));
+const router = jsonServer.router(path.join(process.cwd(), 'src', 'blog_db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
