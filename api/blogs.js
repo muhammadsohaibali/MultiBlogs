6d6d9ae3c015398af54f731b2c492cc62ee06e78
muhammadsohaibali <your-email@example.com>
2025-03-23 16:29:26 +0500

    update

--- api/blogs.js
+++
@@ -1,5 +0,0 @@
-const blogs = require('../../src/blog_db.json');
-
-export default function handler(req, res) {
-    res.status(200).json(blogs);
-}
--- api/users.js
+++
@@ -1,5 +0,0 @@
-const users = require('../../src/users_db.json');
-
-export default function handler(req, res) {
-    res.status(200).json(users);
-}
